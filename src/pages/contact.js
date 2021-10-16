4beadc4484bbf80ea0580e928a0dec3c30b4d58d
Adel Muursepp <adelmuursepp@utmuser165-239.wireless.utoronto.ca>
2021-10-16 13:40:31 -0400

    Added netlify example form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,6 +20,26 @@ const ContactPage = ({
           <p>Let me help you kick start your next project &rarr;</p>
         </div>
         <div>
+        <form name="contact-example" method="POST" data-netlify="true">
+          <p>
+            <label>Your Name: <input type="text" name="name" /></label>   
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email" /></label>
+          </p>
+          <p>
+            <label>Your Role: <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
           <form className="form-container" method="post" data-netlify="true" name="contact">
             <div>
               <label >Name</label>
