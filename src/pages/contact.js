4e39cbe062ddac999523db65d48c042643896dff
Adel Muursepp <adelmuursepp@utmuser165-239.wireless.utoronto.ca>
2021-10-16 13:35:18 -0400

    Added netlify form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,21 +20,21 @@ const ContactPage = ({
           <p>Let me help you kick start your next project &rarr;</p>
         </div>
         <div>
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post" data-netlify="true">
+          <form className="form-container" method="post" data-netlify="true" name="contact">
             <div>
-              <label htmlFor="w3lName">Name</label>
+              <label >Name</label>
               <input type="text" name="w3lName" id="w3lName"/>
             </div>
             <div>
-              <label htmlFor="w3lSender">Email</label>
+              <label >Email</label>
               <input type="email" name="w3lSender" id="w3lSender"/>
             </div>
             <div>
-              <label htmlFor="w3lSubject">Subject</label>
+              <label >Subject</label>
               <input type="text" name="w3lSubject" id="w3lSubject"/>
             </div>
             <div>
-              <label htmlFor="w3lMessage">Message</label>
+              <label >Message</label>
               <textarea name="w3lMessage" id="w3lMessage"></textarea>
             </div>
             <div style={{display: "flex", justifyContent: "flex-end"}}>
